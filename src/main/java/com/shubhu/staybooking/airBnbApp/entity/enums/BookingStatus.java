75400782174bd25fdbbb66a4893a8b44603b474f
Shubhangi Rajput <ranashubhangi2@gmail.com>
2026-01-09 22:55:09 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/shubhu/staybooking/airBnbApp/entity/enums/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.shubhu.staybooking.airBnbApp.entity.enums;
+
+public enum BookingStatus {
+    RESERVED,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ src/main/java/com/shubhu/staybooking/airBnbApp/entity/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.shubhu.staybooking.airBnbApp.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
